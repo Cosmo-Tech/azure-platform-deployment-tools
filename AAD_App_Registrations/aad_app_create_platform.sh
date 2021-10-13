2a0db1beeaa06c56ed10fba8a03ad3036f487fbb
Vincent Carluer <vcarluer@gmail.com>
2021-10-13 18:47:27 +0200

    WIP create platform app registration

---
+++ AAD_App_Registrations/aad_app_create_platform.sh
@@ -0,0 +1,37 @@
+export CUSTOMER=$1
+if [[ -z "$CUSTOMER" ]]; then
+  echo Please provide customer name as first parameter
+  exit 1
+fi
+export IDENTIFIER_URI=$2
+if [[ -z "$IDENTIFIER_URI" ]]; then
+  echo Please provide the base path identifier for oauth2 scope. example: https://dev.api.cosmotech.com
+  exit 1
+fi
+
+echo Creating App Registration...
+app=$(az ad app create --display-name "Cosmo Tech platform for $CUSTOMER" --app-roles @platform_app_roles_manifest.json --available-to-other-tenants --oauth2-allow-implicit-flow true)
+export appId=$(echo $app | jq -r '.appId')
+echo App Registration created: $appId
+echo Creating associated Service Principal
+az ad sp create --id $appId
+echo
+echo creating application scope base identifier
+az ad app update --id $appId --set identifierUris="[ \"$IDENTIFIER_URI\" ]"
+echo disabling default scope
+export default_authz=$(az ad app show --id $appId  | jq '.oauth2Permissions[0].isEnabled = false'  | jq -r '.oauth2Permissions')
+az ad app update --id $appId --set oauth2Permissions="$default_authz"
+echo adding Cosmo Tech platform scope
+az ad app update --id $appId --set oauth2Permissions=@platform_oauth2Permissions_manifest.json
+echo
+echo Adding and granting API Delegated Permission for Microsoft Graph User.Read scope
+az ad app permission add --id $appId --api 00000002-0000-0000-c000-000000000000 --api-permissions 311a71cc-e848-46a1-bdf8-97ff7156d8e6=Scope
+az ad app permission grant --id $appId --api 00000002-0000-0000-c000-000000000000
+echo Adding and granting API Application Permission on itself as Platform.Admin role
+az ad app permission add --id $appId --api $appId --api-permissions bb49d61f-8b6a-4a19-b5bd-06a29d6b8e60=Role
+az ad app permission grant --id $appId --api $appId
+echo
+echo Granting admin consent...
+az ad app permission admin-consent --id $appId
+echo Admin consent done
+
