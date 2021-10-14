37ab70a6d7fe167adafae7aa71b5fa01573fafc6
Vincent Carluer <vcarluer@gmail.com>
2021-10-14 09:10:22 +0200

    script echos

--- AAD_App_Registrations/Platform/aad_app_create_platform.sh
+++ AAD_App_Registrations/Platform/aad_app_create_platform.sh
@@ -12,22 +12,22 @@ fi
 export PLATFORM_URL=$3
 if [[ -z "$PLATFORM_URL" ]]; then
   export PLATFORM_URL=$IDENTIFIER_URI
-  echo No platform URL set, using identifier uri $IDENTIFIER_URI
+  echo No specific API platform URL set, using identifier uri $IDENTIFIER_URI
 fi
 
 echo Creating App Registration...
-app=$(az ad app create --display-name "Cosmo Tech platform for $CUSTOMER" --app-roles @platform_app_roles_manifest.json --available-to-other-tenants --reply-urls $PLATFORM_URL/swagger-ui/oauth2-redirect.html --oauth2-allow-implicit-flow true)
+app=$(az ad app create --display-name "Cosmo Tech Platform For $CUSTOMER" --app-roles @platform_app_roles_manifest.json --available-to-other-tenants --reply-urls $PLATFORM_URL/swagger-ui/oauth2-redirect.html --oauth2-allow-implicit-flow true)
 export appId=$(echo $app | jq -r '.appId')
 echo App Registration created: $appId
 echo Creating associated Service Principal
 az ad sp create --id $appId
 echo
-echo creating application scope base identifier
+echo Creating application scope base identifier
 az ad app update --id $appId --set identifierUris="[ \"$IDENTIFIER_URI\" ]"
-echo disabling default scope
+echo Disabling default scope
 export default_authz=$(az ad app show --id $appId  | jq '.oauth2Permissions[0].isEnabled = false'  | jq -r '.oauth2Permissions')
 az ad app update --id $appId --set oauth2Permissions="$default_authz"
-echo adding Cosmo Tech platform scope
+echo Adding Cosmo Tech platform scope
 az ad app update --id $appId --set oauth2Permissions=@platform_oauth2Permissions_manifest.json
 echo
 echo Adding and granting API Delegated Permission for Microsoft Graph User.Read scope
@@ -36,7 +36,7 @@ az ad app permission grant --id $appId --api 00000002-0000-0000-c000-00000000000
 echo Adding and granting API Application Permission on itself as Platform.Admin role
 az ad app permission add --id $appId --api $appId --api-permissions bb49d61f-8b6a-4a19-b5bd-06a29d6b8e60=Role
 az ad app permission grant --id $appId --api $appId --consent-type AllPrincipals
-echo waiting 60s for admin consent....
+echo Waiting 60s for admin consent....
 sleep 60s
 echo Granting admin consent...
 az ad app permission admin-consent --id $appId
