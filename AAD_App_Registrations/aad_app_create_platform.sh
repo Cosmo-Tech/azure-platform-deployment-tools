6161e07a2c05a587e7191896ad9022cfa85fc71c
Vincent Carluer <vcarluer@gmail.com>
2021-10-14 09:05:27 +0200

    Working version of app platform

--- AAD_App_Registrations/aad_app_create_platform.sh
+++ AAD_App_Registrations/aad_app_create_platform.sh
@@ -9,8 +9,14 @@ if [[ -z "$IDENTIFIER_URI" ]]; then
   exit 1
 fi
 
+export PLATFORM_URL=$3
+if [[ -z "$PLATFORM_URL" ]]; then
+  export PLATFORM_URL=$IDENTIFIER_URI
+  echo No platform URL set, using identifier uri $IDENTIFIER_URI
+fi
+
 echo Creating App Registration...
-app=$(az ad app create --display-name "Cosmo Tech platform for $CUSTOMER" --app-roles @platform_app_roles_manifest.json --available-to-other-tenants --oauth2-allow-implicit-flow true)
+app=$(az ad app create --display-name "Cosmo Tech platform for $CUSTOMER" --app-roles @platform_app_roles_manifest.json --available-to-other-tenants --reply-urls $PLATFORM_URL/swagger-ui/oauth2-redirect.html --oauth2-allow-implicit-flow true)
 export appId=$(echo $app | jq -r '.appId')
 echo App Registration created: $appId
 echo Creating associated Service Principal
@@ -26,12 +32,12 @@ az ad app update --id $appId --set oauth2Permissions=@platform_oauth2Permissions
 echo
 echo Adding and granting API Delegated Permission for Microsoft Graph User.Read scope
 az ad app permission add --id $appId --api 00000002-0000-0000-c000-000000000000 --api-permissions 311a71cc-e848-46a1-bdf8-97ff7156d8e6=Scope
-az ad app permission grant --id $appId --api 00000002-0000-0000-c000-000000000000
+az ad app permission grant --id $appId --api 00000002-0000-0000-c000-000000000000 --consent-type AllPrincipals --scope User.Read
 echo Adding and granting API Application Permission on itself as Platform.Admin role
 az ad app permission add --id $appId --api $appId --api-permissions bb49d61f-8b6a-4a19-b5bd-06a29d6b8e60=Role
-az ad app permission grant --id $appId --api $appId
-echo
+az ad app permission grant --id $appId --api $appId --consent-type AllPrincipals
+echo waiting 60s for admin consent....
+sleep 60s
 echo Granting admin consent...
 az ad app permission admin-consent --id $appId
 echo Admin consent done
-
