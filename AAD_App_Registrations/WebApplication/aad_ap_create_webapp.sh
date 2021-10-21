e543bdac8d0df06605c8617eb31a11e35199719e
Vincent Carluer <vcarluer@gmail.com>
2021-10-21 13:28:01 +0200

    use service principal to get API scope guid + option for dev authentication url

--- AAD_App_Registrations/WebApplication/aad_ap_create_webapp.sh
+++ AAD_App_Registrations/WebApplication/aad_ap_create_webapp.sh
@@ -1,4 +1,4 @@
-echo usage: ./aad_ap_create_webapp.sh CUSTOMER PLATFORM_APP_ID WEBAPP_URL [STAGE=Dev] [PROJECT]
+echo usage: ./aad_ap_create_webapp.sh CUSTOMER PLATFORM_APP_ID WEBAPP_URL [STAGE=Dev] [PROJECT] [DEVAUTHENT=True]
 export CUSTOMER=$1
 if [[ -z "$CUSTOMER" ]]; then
   echo Please provide customer name as first parameter
@@ -29,6 +29,19 @@ else
   export PROJECT=" - $PROJECT"
 fi
 
+export DEVAUTHENT=$6
+if [[ -z "$DEVAUTHENT" ]]; then
+  export DEVAUTHENT=true
+  echo Dev authentication authorized
+fi
+
+if [[ "$DEVAUTHENT" == "true" ]]; then
+  export authentUri="[\"${WEBAPP_URL}/scenario\", \"http://localhost:3000/scenario\"]"
+else
+  export authentUri="[\"${WEBAPP_URL}/scenario\"]"
+fi
+
+
 
 echo Creating App Registration...
 app=$(az ad app create --display-name "Cosmo Tech Web Application For ${STAGE} - ${CUSTOMER}${PROJECT}")
@@ -43,10 +56,15 @@ az ad sp update --id $appId --add tags "WindowsAzureActiveDirectoryIntegratedApp
 echo
 echo Adding Authentication SPA Platform with authorized redirect URI
 # it is not possible to update directly replyUrlsWithType with az ad app update and set SPA reply url another way than REST API (10/2021)
-az rest --method PATCH --uri "https://graph.microsoft.com/v1.0/applications/${appObjectId}" --headers "Content-Type=application/json" --body "{\"spa\":{\"redirectUris\":[\"${WEBAPP_URL}/scenario\"]}}"
+
+az rest --method PATCH --uri "https://graph.microsoft.com/v1.0/applications/${appObjectId}" --headers "Content-Type=application/json" --body "{\"spa\":{\"redirectUris\":${authentUri}}}"
 echo Adding and granting API Delegated Permission for Microsoft Graph User.Read scope
 az ad app permission add --id $appId --api 00000003-0000-0000-c000-000000000000 --api-permissions e1fe6dd8-ba31-4d61-89e7-88639da4683d=Scope
 az ad app permission grant --id $appId --api 00000003-0000-0000-c000-000000000000 --consent-type AllPrincipals --scope User.Read
+echo Getting Platform Enterprise Application OAuth2 informations
+export platformEA=$(az ad sp show --id ${PLATFORM_APP_ID})
+export scopeId=$(echo $platformEA | jq -r '.oauth2Permissions[0].id')
 echo Adding and granting API Delegated Permission for Cosmo Tech API Platform - platform scope
-az ad app permission add --id $appId --api $PLATFORM_APP_ID --api-permissions 6332363e-bcba-4c4a-a605-c25f23117400=Scope
+az ad app permission add --id $appId --api $PLATFORM_APP_ID --api-permissions ${scopeId}=Scope
+echo API scope if is ${scopeId}
 az ad app permission grant --id $appId --api $PLATFORM_APP_ID --consent-type AllPrincipals --scope platform
