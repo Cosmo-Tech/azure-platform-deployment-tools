15e410403e35ed8d1001767e7f8262c42a7ab636
Vincent Carluer <vcarluer@gmail.com>
2022-09-22 16:08:52 +0200

    style and vnet id

--- install_scripts/az_login_customer_sp.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-echo "Login with Azure using Service Principal credentials: ${CUSTOMER_SERVICE_PRINCIPAL_APPID}";
-az login --service-principal -u "${CUSTOMER_SERVICE_PRINCIPAL_APPID}" -p "${CUSTOMER_SERVICE_PRINCIPAL_SECRET}" --tenant "${TENANT}" 2>&1 || exit 1;
--- install_scripts/create_private_dns_zone.sh
+++ install_scripts/create_private_dns_zone.sh
@@ -1,14 +1,20 @@
 #!/usr/bin/env bash
+# Copyright (c) Cosmo Tech.
+# Licensed under the MIT license.
+echo create_private_dns_zone.sh start
 ./az_login_customer_sp.sh
 
 echo Creating private DNS zone ${ZONE_NAME}...
-az network private-dns zone create --resource-group ${RESOURCE_GROUP} \
-   --name  $ZONE_NAME
+az network private-dns zone create \
+  --resource-group ${RESOURCE_GROUP} \
+  --name  ${ZONE_NAME}
 
-echo Creating private DNS link ${ZONE_LINK_NAME}...
-az network private-dns link vnet create --resource-group ${RESOURCE_GROUP} \
-   --zone-name  $ZONE_NAME\
-   --name ${ZONE_LINK_NAME} \
-   --virtual-network $VNetName \
-   --registration-enabled false 
+echo Creating private DNS link ${ZONE_LINK_NAME} for ${ZONE_NAME} to ${VNET_ID}...
+az network private-dns link vnet create \
+  --resource-group ${RESOURCE_GROUP} \
+  --zone-name ${ZONE_NAME} \
+  --name ${ZONE_LINK_NAME} \
+  --virtual-network ${VNET_ID} \
+  --registration-enabled false 
 
+echo create_private_dns_zone.sh end
