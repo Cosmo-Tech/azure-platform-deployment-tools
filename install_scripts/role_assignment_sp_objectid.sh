9f6cfc3d8f77801b11d9a5018a5cf5a53a115c57
Vincent Carluer <vcarluer@gmail.com>
2022-09-22 19:23:57 +0200

    use sp id

--- install_scripts/role_assignment_sp_objectid.sh
+++ install_scripts/role_assignment_sp_objectid.sh
@@ -3,17 +3,17 @@
 # Licensed under the MIT license.
 set -x
 echo role_assignment_objectid.sh start
-echo SP_NAME: the service principal name to assign role to
+echo SP_ID: the service principal id to assign role to
 echo ROLE_ID: the Azure role UID to assign
 echo RESOURCE_SCOPE: the resource scope to assign role to
 echo
 
-echo Assigning ${ROLE_ID} to ${SP_NAME} on ${RESOURCE_SCOPE}
+echo Assigning ${ROLE_ID} to ${SP_ID} on ${RESOURCE_SCOPE}
 az role assignment create \
-  --assignee "${SP_NAME}" \
+  --assignee-object-id "${SP_ID}" \
   --role "${ROLE_ID}" \
   --scope "${RESOURCE_SCOPE}" \
-  --assignee-principal-type "ServicePrincipal"
+  --assignee-principal-type "ServicePrincipal" \
   2>&1 || exit 1;
 
 echo role_assignment_objectid.sh end
