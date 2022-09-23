4819fee6025b17094051dc8308f0bab0829f62f1
Vincent Carluer <vcarluer@gmail.com>
2022-09-23 16:40:46 +0200

    zones for adx pep

--- install_scripts/create_private_endpoint_adx.sh
+++ install_scripts/create_private_endpoint_adx.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # Copyright (c) Cosmo Tech.
 # Licensed under the MIT license.
-echo create_private_endpoint.sh start
+echo create_private_endpoint_adx.sh start
 echo Mandatory env vars:
 echo SP_APPID: the service principal application / client id
 echo SP_SECRET: the service principal secret
@@ -13,7 +13,10 @@ echo RESOURCE_ID: The resource id linked to the private endpoint
 echo GROUP_ID: The resource group to link
 echo CONNECTION_NAME: The private endpoint connection name
 echo DNS_GROUP_NAME: The DNS zone group name
-echo ZONE_NAME: The DNS private zone name
+echo ZONE_NAME_ADX: The DNS private zone name for ADX
+echo ZONE_NAME_BLOB: The DNS private zone name for blob
+echo ZONE_NAME_QUEUE: The DNS private zone name for queue
+echo ZONE_NAME_TABLE: The DNS private zone name for table
 echo
 source ./az_login_as_sp.sh
 
@@ -27,14 +30,41 @@ az network private-endpoint create \
   --connection-name ${CONNECTION_NAME} \
   2>&1 || exit 1
 
-echo Creating private DNS group ${DNS_GROUP_NAME} in ${ZONE_NAME}...
+echo Creating private DNS group ${DNS_GROUP_NAME} in ${ZONE_NAME} for ADX cluster...
 az network private-endpoint dns-zone-group create \
   --resource-group ${RESOURCE_GROUP} \
   --endpoint-name ${PRIVATE_ENDPOINT_NAME} \
   --name ${DNS_GROUP_NAME} \
-  --private-dns-zone ${ZONE_NAME} \
-  --zone-name ${ZONE_NAME} \
+  --private-dns-zone ${ZONE_NAME_ADX} \
+  --zone-name ${ZONE_NAME_ADX} \
   2>&1 || exit 1
 
-echo create_private_endpoint.sh end
+echo Creating private DNS group ${DNS_GROUP_NAME} in ${ZONE_NAME} for ADX blob...
+az network private-endpoint dns-zone-group create \
+  --resource-group ${RESOURCE_GROUP} \
+  --endpoint-name ${PRIVATE_ENDPOINT_NAME} \
+  --name ${DNS_GROUP_NAME} \
+  --private-dns-zone ${ZONE_NAME_BLOB} \
+  --zone-name ${ZONE_NAME_BLOB} \
+  2>&1 || exit 1
+
+echo Creating private DNS group ${DNS_GROUP_NAME} in ${ZONE_NAME} for ADX queue...
+az network private-endpoint dns-zone-group create \
+  --resource-group ${RESOURCE_GROUP} \
+  --endpoint-name ${PRIVATE_ENDPOINT_NAME} \
+  --name ${DNS_GROUP_NAME} \
+  --private-dns-zone ${ZONE_NAME_QUEUE} \
+  --zone-name ${ZONE_NAME_QUEUE} \
+  2>&1 || exit 1
+
+echo Creating private DNS group ${DNS_GROUP_NAME} in ${ZONE_NAME} for ADX table...
+az network private-endpoint dns-zone-group create \
+  --resource-group ${RESOURCE_GROUP} \
+  --endpoint-name ${PRIVATE_ENDPOINT_NAME} \
+  --name ${DNS_GROUP_NAME} \
+  --private-dns-zone ${ZONE_NAME_TABLE} \
+  --zone-name ${ZONE_NAME_TABLE} \
+  2>&1 || exit 1
+
+echo create_private_endpoint_adx.sh end
 
