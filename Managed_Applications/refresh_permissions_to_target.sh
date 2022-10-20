afb4915b01a6ec067b7cadbdd0993d52afbad474
Vincent Carluer <vcarluer@gmail.com>
2022-10-20 10:39:32 +0200

    add refresh permission to target

---
+++ Managed_Applications/refresh_permissions_to_target.sh
@@ -0,0 +1,1 @@
+az rest --method post --uri "/subscriptions/${SUB}/resourceGroups/${MARG}/providers/Microsoft.Solutions/applications/${MA}/refreshPermissions?targetVersion=${MAVER}&api-version=2019-07-01"
