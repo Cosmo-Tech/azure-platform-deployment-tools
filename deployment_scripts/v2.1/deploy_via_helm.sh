89f3a231b0a9197487a4b118498d695f5506f17f
Vincent Carluer <vcarluer@gmail.com>
2023-01-03 11:18:05 +0100

    add Gi suffix if does not exist for redis disk size

--- deployment_scripts/v2.1/deploy_via_helm.sh
+++ deployment_scripts/v2.1/deploy_via_helm.sh
@@ -373,7 +373,9 @@ EXISTING_REDIS_PV_NAME=$(kubectl get persistentvolumes -n "${NAMESPACE}" -l "cos
 REDIS_PV_NAME=cosmotech-database-master-pv
 REDIS_PVC_NAME="${REDIS_MASTER_NAME_PVC:-"cosmotech-database-master-pvc"}"
 
-[[ ${REDIS_DISK_SIZE} == *Gi ]] || ${REDIS_DISK_SIZE}+=Gi
+[[ ${REDIS_DISK_SIZE} != *Gi ]]; then
+  export REDIS_DISK_SIZE=${REDIS_DISK_SIZE}Gi
+fi
 
 if [[ "${EXISTING_REDIS_PV_NAME:-}" == "" && "${REDIS_DISK_RESOURCE:-}" != "" ]]; then
 
