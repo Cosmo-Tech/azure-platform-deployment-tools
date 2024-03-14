1cef88becf7c9746c4ad36372bdd0db4756a0e7f
Jérémy Reynard <jeremy.reynard@cosmotech.com>
2024-03-14 09:04:19 +0100

    Update deploy_via_helm.sh
    Change logging level to WARN

--- deployment_scripts/v3.1/deploy_via_helm.sh
+++ deployment_scripts/v3.1/deploy_via_helm.sh
@@ -1075,10 +1075,10 @@ image:
 config:
   logging:
     level:
-      com.cosmotech: TRACE
-      web: TRACE
-      org.springframework: TRACE
-      com.redis: TRACE
+      com.cosmotech: WARN
+      web: WARN
+      org.springframework: WARN
+      com.redis: WARN
   server:
     error:
       include-stacktrace: always
